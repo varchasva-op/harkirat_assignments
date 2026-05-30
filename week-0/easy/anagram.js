ae5f081a7421463d895e0d6e67088b21c70d41f8
varchasva-op <varchasva.op23@gmail.com>
2026-05-30 06:19:22 +0530

    Update anagram.js

--- week-0/easy/anagram.js
+++ week-0/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+    // 1. Dono strings ko lowercase karke unke saare spaces hatao
+    const cleanStr1 = str1.toLowerCase().replace(/\s+/g, '');
+    const cleanStr2 = str2.toLowerCase().replace(/\s+/g, '');
 
+    // 2. Agar length barabar nahi hai, to anagram nahi ho sakta
+    if (cleanStr1.length !== cleanStr2.length) {
+        return false;
+    }
+
+    // 3. Characters ko split karo, sort karo, aur wapas join karke compare karo
+    const sortedStr1 = cleanStr1.split('').sort().join('');
+    const sortedStr2 = cleanStr2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
